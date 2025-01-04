ff7c26f6dc5d14cba2025f3e6480468a093bbda0
alvaro05p <alvaro05pomg@gmail.com>
2025-01-04 13:33:39 +0100

    proyecto finalizado

---
+++ proyecto/script/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("boton").onclick = function(){
+
+    let mostrar = document.getElementById("mostrar");
+
+    mostrar.classList.toggle("desplegado");
+    mostrar.classList.toggle("no_mostrar");
+    
+}
